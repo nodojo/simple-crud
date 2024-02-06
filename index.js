cb6825ffcae2bba53918f3038bcb50a2053d1733
nodojo <nulldojo@gmail.com>
2024-02-05 23:25:44 -0500

    add delete user endpoint and export
    add route to index.js to call delete user endpoint

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
   console.log(`Application running on port ${port}.`)
