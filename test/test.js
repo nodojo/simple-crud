50e76886fc350e9c6a63b5594dc665872405cb54
nodojo <nulldojo@gmail.com>
2024-02-07 08:53:39 -0500

    add test to validate response type and status code for updateUser  endpoint on success

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../index')
 
 describe('GET /users', function() {
-  it('responds with json', function(done) {
+  it('returns all users', function(done) {
     request(app)
     .get('/users')
     .set('Accept', 'application/json')
@@ -12,7 +12,7 @@ describe('GET /users', function() {
 });
 
 describe('GET /users/:id', function() {
-  it('returns a user', function(done) {
+  it('returns a user by id', function(done) {
     request(app)
     .get('/users/1')
     .set('Accept', 'application/json')
@@ -25,7 +25,7 @@ describe('GET /users/:id', function() {
 })
 
 describe('POST /users', function() {
-  it('responds with json', function(done) {
+  it('creates a user by accepting name and email', function(done) {
     request(app)
     .post('/users')
     .send({name: 'john',email: 'john@test.com'})
@@ -38,3 +38,18 @@ describe('POST /users', function() {
     });
   });
 });
+
+describe('PUT /users/:id', function() {
+    it('updates a user by id', function(done) {
+      request(app)
+      .put('/users/2')
+      .send({name: 'jane',email: 'jane@test.com'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        return done();
+      });
+    })
+})
